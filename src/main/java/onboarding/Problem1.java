67141bd7042b4aca9aeaa0b4fee3f863395ad2d2
jongwankim <jongbell4@gmail.com>
2024-04-09 16:43:20 +0900

    feat: 기본 뼈대

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -9,7 +9,7 @@ public class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         // 00. 시작 면이나 마지막 면이 나오도록 책을 펼치지 않는다.(예외사항)
-        //if(startCondition(pobi) && startCondition(crong)) return -1;
+        if(startCondition(pobi) && startCondition(crong)) return -1;
 
         // 02. 왼쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다.
         // 03. 오른쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다.
@@ -26,6 +26,19 @@ public class Problem1 {
         return answer;
     }
 
+    /*각 자리수 합 (재귀 호출)*/
+    private static Integer plusDigit(Integer parameter){
+
+    }
+
+    /*각 자리수 곱 (재귀 호출)*/
+    private static Integer multipleDigit(Integer parameter){
+
+    }
+
+    private static Integer findBiggerValue(Integer parameter1, Integer parameter2){
+
+    }
 
     private static boolean startCondition(List<Integer> target){
         return !target.contains(1) && !target.contains(400);
@@ -55,7 +68,6 @@ public class Problem1 {
             param = param / 10;
             cnt++;
         }
-
         return result;
     }
 }
