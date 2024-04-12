3790ab2f520be0ed48ffad58d37f07e5f23af665
jongwankim <jongbell4@gmail.com>
2024-04-12 18:49:43 +0900

    feat: case 2 일단 생각나는대로 작성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,17 +1,35 @@
 package onboarding;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Problem2 {
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = cryptogram;
         //00. 문자열 -> char[]
         //01. 인접한 두 원소 비교 (buble)
-        //01-1. 두 문자가 다를 경우 -> index++
-        //01-2. 두 문자가 같을 경우 -> 인접한 두 문자 제거
-        //02. 반복실 행
+        int i = 0;
+        while (answer.length()-1 > i){
+            if(Problem2.인접한_두_원소_비교(answer.charAt(i), answer.charAt(i+1))){
+                //01-2. 두 문자가 같을 경우 -> 인접한 두 문자 제거
+                answer = Problem2.인접한_두_원소_제거(answer, i);
+                i = 0;
+            }else{
+                //01-1. 두 문자가 다를 경우 -> index++
+                i++;
+            }
+        }
         return answer;
     }
 
-    public static String 연달은_중복_문자열_제거(String parameter){
-
+    public static boolean 인접한_두_원소_비교(char a, char b){
+        return a == b;
     }
+
+    public static String  인접한_두_원소_제거(String parameter, int index){
+        StringBuilder sb = new StringBuilder(parameter);
+        return sb.deleteCharAt(index).deleteCharAt(index).toString();
+    }
+
 }
