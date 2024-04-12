576711bef18dac1c5084418d934fd19385364012
jongwankim <jongbell4@gmail.com>
2024-04-12 17:52:07 +0900

    feat: Problem2 실행 순서

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,11 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+        //00. 문자열 -> char[]
+        //01. 인접한 두 원소 비교 (buble)
+        //01-1. 두 문자가 다를 경우 -> index++
+        //01-2. 두 문자가 같을 경우 -> 인접한 두 문자 제거
+        //02. 반복실 행
         return answer;
     }
 
