71515727a05f6f3f9c39e2b2d2b3b998c37d3f08
jongwankim <jongbell4@gmail.com>
2024-04-12 17:28:21 +0900

    feat: case3 경우 고려

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -8,13 +8,32 @@ public class Problem1 {
     // 01. 책음 임의로 펼친다. -> 인스턴스 생성
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        if(Problem1.시작_조건_확인(pobi) || Problem1.시작_조건_확인(crong)) return -1;
 
-        return answer;
+        Integer pobiScore = 0;
+        Integer pobiL = pobi.get(0);
+        Integer pobiR = pobi.get(1);
+
+        Integer crongScore = 0;
+        Integer crongL = crong.get(0);
+        Integer crongR = crong.get(1);
+
+        pobiScore = Problem1.두_값중_더_큰_값_리턴(
+                Problem1.두_값중_더_큰_값_리턴(Problem1.각_자리수_합(pobiL),Problem1.각_자리수_곱(pobiL)),
+                Problem1.두_값중_더_큰_값_리턴(Problem1.각_자리수_합(pobiR),Problem1.각_자리수_곱(pobiR)));
+
+        crongScore = Problem1.두_값중_더_큰_값_리턴(
+                Problem1.두_값중_더_큰_값_리턴(Problem1.각_자리수_합(crongL),Problem1.각_자리수_곱(crongL)),
+                Problem1.두_값중_더_큰_값_리턴(Problem1.각_자리수_합(crongR),Problem1.각_자리수_곱(crongR)));
+
+        if (pobiScore > crongScore) return 1;
+        else if(pobiScore < crongScore) return 2;
+        else return 0;
     }
 
     //시작 조건 검사
     public static boolean 시작_조건_확인(List<Integer> parameter){
-        return !( parameter.contains(1) || parameter.contains(400) );
+        return ( parameter.contains(1) || parameter.contains(400) );
     }
 
     //각 자리수 합 (재귀 호출)
