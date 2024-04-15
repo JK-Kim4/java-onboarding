3694a03c87d7e1eee892fadd87b8605b8d0addcd
jongwankim <jongbell4@gmail.com>
2024-04-15 17:23:34 +0900

    feat: Problem3 regex -> 중복값(예:33) 카운트 오류

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,22 @@
 package onboarding;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        Pattern pattern = Pattern.compile("([369])");
+
+        for(int i = 1; i <= number; i++){
+            Matcher matcher = pattern.matcher(String.valueOf(i));
+            if (matcher.find()){
+                System.out.println(i);
+                answer++;
+            }
+        }
+
         return answer;
     }
 }
