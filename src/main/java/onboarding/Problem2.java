639e802f04c9cf86b360244b1577df2d738539b7
jongwankim <jongbell4@gmail.com>
2024-04-15 16:41:55 +0900

    feat: stack 응용

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,35 +1,42 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Problem2 {
 
     public static String solution(String cryptogram) {
-        String answer = cryptogram;
-        //00. 문자열 -> char[]
-        //01. 인접한 두 원소 비교 (buble)
-        int i = 0;
-        while (answer.length()-1 > i){
-            if(Problem2.인접한_두_원소_비교(answer.charAt(i), answer.charAt(i+1))){
-                //01-2. 두 문자가 같을 경우 -> 인접한 두 문자 제거
-                answer = Problem2.인접한_두_원소_제거(answer, i);
-                i = 0;
+        //stack 응용
+        StringBuilder sb = new StringBuilder();
+        List<Character> resultList = new ArrayList<>();
+
+        for(char c: cryptogram.toCharArray()){
+            if(resultList.size() == 0){
+                resultList.add(c);
             }else{
-                //01-1. 두 문자가 다를 경우 -> index++
-                i++;
+                if(resultList.get(resultList.size()-1) == c){
+                    resultList.remove(resultList.size()-1);
+                }else {
+                    resultList.add(c);
+                }
             }
         }
-        return answer;
+
+        for(Character c: resultList){
+            sb.append(c);
+        }
+
+        return sb.toString();
     }
 
-    public static boolean 인접한_두_원소_비교(char a, char b){
-        return a == b;
-    }
-
-    public static String  인접한_두_원소_제거(String parameter, int index){
-        StringBuilder sb = new StringBuilder(parameter);
-        return sb.deleteCharAt(index).deleteCharAt(index).toString();
-    }
+//    public static boolean 인접한_두_원소_비교(char a, char b){
+//        return a == b;
+//    }
+//
+//    public static String  인접한_두_원소_제거(String parameter, int index){
+//        StringBuilder sb = new StringBuilder(parameter);
+//        return sb.deleteCharAt(index).deleteCharAt(index).toString();
+//    }
 
 }
