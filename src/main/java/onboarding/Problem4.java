5440fa3b47ff013ef6ec40f6b731c03f78099799
jongwankim <jongbell4@gmail.com>
2024-04-18 14:03:56 +0900

    feat: problem4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,21 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+
+
         return answer;
+    }
+
+    public static int 문자를_받아_아스키코드로_변환(char c){
+        return 0;
+    }
+
+    public static int 아스키코드를_받아_변환된_코드_리턴(int org){
+        return 0;
+    }
+
+    public static char 아스키코드를_받아_문자로_변환(int orgAscii){
+        return ' ';
     }
 }
